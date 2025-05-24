3251c1fe9fc3da062731591527fc34dbcd350c49
Natalia Shishina <nshishina@odin.com>
2025-05-24 02:13:58 -0700

    watch computers play

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app
 
-EXPOSE 5000
+EXPOSE 8080
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from flask import Flask, render_template, request, redirect, url_for, session
 import numpy as np
 from tictactoe import TicTacToe
@@ -37,27 +39,26 @@ def index():
             else:
                 message = 'Cell occupied!'
 
-    # Game loop logic
-    while True:
-        result = t.count_winner()
-        if result:
-            message = result
-            break
+    result = t.count_winner()
+    if result:
+        message = result
+        return render_template('index.html', game=t.game, message=message, players=players, turn=turn, auto_play=False)
 
-        current_player = players[turn % 2]
-        if current_player == 'user':
-            break  # Wait for user input
-        elif current_player == 'easy':
+    current_player = players[turn % 2]
+    auto_play = False
+    if current_player != 'user' and not message:
+        # Computer's turn: make a move, then render with auto_play
+        if current_player == 'easy':
             t.make_easy_move()
         elif current_player == 'medium':
             t.make_medium_move()
         elif current_player == 'hard':
             t.make_hard_move()
         session['game'] = t.game.tolist()
-        turn += 1
-        session['turn'] = turn
+        session['turn'] = turn + 1
+        auto_play = True  # Tell the template to auto-refresh
 
-    return render_template('index.html', game=t.game, message=message, players=players, turn=turn)
+    return render_template('index.html', game=t.game, message=message, players=players, turn=turn, auto_play=auto_play)
 
 @app.route('/select_players', methods=['GET', 'POST'])
 def select_players():
@@ -78,4 +79,4 @@ def reset():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
